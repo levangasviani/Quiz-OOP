56cb98f41576af2b58f5e8cff2661d7d40c4e3e1
levangasviani <lgasv15@freeuni.edu.ge>
2017-06-14 19:29:10 +0400

    Proeqtis struqtura

---
+++ QuizWebsite/src/User/Account.java
@@ -0,0 +1,32 @@
+package User;
+
+public class Account {
+
+	public Account(String username, String password, String firstname, String lastname, String email, int type) {
+
+	}
+
+	public String getUserName() {
+		return null;
+	}
+
+	public String getPassword() {
+		return null;
+	}
+
+	public String getFirstName() {
+		return null;
+	}
+
+	public String getLastName() {
+		return null;
+	}
+
+	public String getEmail() {
+		return null;
+	}
+
+	public int getType() {
+		return 0;
+	}
+}
---
+++ QuizWebsite/src/User/AccountManager.java
@@ -0,0 +1,25 @@
+package User;
+
+public class AccountManager {
+
+	public AccountManager() {
+
+	}
+
+	public void addAccount(Account account) {
+
+	}
+
+	public void addAccount(String username, String password, String firstname, String lastname, String email,
+			int type) {
+
+	}
+
+	boolean containsAccount(String username) {
+		return false;
+	}
+
+	public Account getAccount(String username) {
+		return null;
+	}
+}
