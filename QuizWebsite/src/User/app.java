26cbeb64a283c561c65d9ea0d5b67e89369bd656
levangasviani <lgasv15@freeuni.edu.ge>
2017-06-15 22:35:45 +0400

    Es aris App klasi romelshi main metodia da aqedan AccountManageris metodebis sworad mushaoba shevamowme, klass unit testebitac gavtestavt

---
+++ QuizWebsite/src/User/app.java
@@ -0,0 +1,36 @@
+package User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class app {
+
+	public static void main(String[] args) throws SQLException {
+		AccountManager am = new AccountManager();
+		
+		am.addAccount("levangasviani", "qwerty", "Levani", "Gasviani", "lgasv15@freeuni.edu.ge", 2);
+		am.addAccount("shotakobaxidze", "123456", "Shota", "Kobaxidze", "skoba15@freeuni.edu.ge", 2);
+		am.addAccount("levankaranadze", "asdasd", "Levani", "Karanadze", "lkara15@freeuni.edu.ge", 2);
+		am.addAccount("lashakharshiladze", "cubismama", "Lasha", "Kharshiladze", "lkhar15@freeuni.edu.ge", 2);
+		am.addAccount("sandrotsiskadze", "koloriti777", "Sandro", "Tsiskadze", "stsis15@freeuni.edu.ge", 2);
+		
+		Account acc1 = am.getAccount("levangasviani");
+		Account acc2 = am.getAccount("shotakobaxidze");
+		Account acc3 = am.getAccount("levankaranadze");
+		Account acc4 = am.getAccount("lashakharshiladze");
+		Account acc5 = am.getAccount("sandrotsiskadze");
+		
+		System.out.println(acc1.toString());
+		System.out.println(acc2.toString());
+		System.out.println(acc3.toString());
+		System.out.println(acc4.toString());
+		System.out.println(acc5.toString());
+		
+		ArrayList<Account> al = am.getAccountsList();
+		for(Account a : al) {
+			System.out.println(a.toString());
+		}
+		
+	}
+
+}
