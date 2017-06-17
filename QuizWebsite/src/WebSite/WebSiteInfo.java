4062e4c5fdd22c208a7f74b85b1f559c76aa8a68
Lasha Kharshiladze <hunger-sona@mail.ru>
2017-06-17 15:51:12 +0400

    დავამაე WebSiteInfo კლასსში DEBUG_MODE მანიშნებელი რომელიც ჭეშმარიტი მნიშვნელობაც უზრუნველყოფს სხვადასხვა ადგილებში საჭირო ინფორმაციების დაბეჭვდას

--- QuizWebsite/src/WebSite/WebSiteInfo.java
+++ QuizWebsite/src/WebSite/WebSiteInfo.java
@@ -1,8 +1,16 @@
 package WebSite;
-
+/**
+ * Class WebSiteInfo:
+ * WebSiteInfo is class that saves names of site content
+ * html,jsp, servlets and attributes in servletContexst
+ * 
+ * @author Lasha Kharshiladze
+ */
 public  class WebSiteInfo {
-	private static final String LOGIN_PAGE			 = "index.jsp";
-	private static final String REGISTRATION_PAGE	 = "registration.jsp";
-	private static final String REGISTRATION_SERVLET = "RegistrationServlet";
-	private static final String LOGIN_SERVLET 		 = "LoginServlet";
+	public static final String  LOGIN_PAGE			  = "index.html";
+	public static final String  REGISTRATION_PAGE	  = "index.html";
+	public static final String  REGISTRATION_SERVLET  = "RegistrationServlet";
+	public static final String  LOGIN_SERVLET 		  = "LoginServlet";
+	public static final String  ACCOUNT_MANAGER_ATTR  = "AccountManager";
+	public static final boolean DEBUG_MODE            = true; 
 }
