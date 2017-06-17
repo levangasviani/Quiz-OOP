6efdc8ad7cebb3592dd3a0d4d9ebfa59204eb05e
Lasha Kharshiladze <hunger-sona@mail.ru>
2017-06-17 12:31:30 +0400

    შევქმენი კლასი რომელშიც შევინახავთ გლობალურ ინფორმაციას საიტის შესახებ კერძოდ html, jsp ფაილების სახელებს რათა მათი მენეჯმენტი უფრო მოსახერხებელი იყოს

---
+++ QuizWebsite/src/WebSite/WebSiteInfo.java
@@ -0,0 +1,8 @@
+package WebSite;
+
+public  class WebSiteInfo {
+	private static final String LOGIN_PAGE			 = "index.jsp";
+	private static final String REGISTRATION_PAGE	 = "registration.jsp";
+	private static final String REGISTRATION_SERVLET = "RegistrationServlet";
+	private static final String LOGIN_SERVLET 		 = "LoginServlet";
+}
