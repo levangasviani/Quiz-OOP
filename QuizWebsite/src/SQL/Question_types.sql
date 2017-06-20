d92a5ae6dd6631a0a4a2f14a886d14a9ac84d5cd
shotakobaxidze <skoba15@freeuni.edu.ge>
2017-06-20 13:46:06 +0300

    davamate question tipebis insertebi testirebistvis

---
+++ QuizWebsite/src/SQL/Question_types.sql
@@ -0,0 +1,7 @@
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (1, 'Question-Response');
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (2, 'Fill_In_The_Blank');
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (3, 'Multiple_Choice');
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (4, 'Picture-Response');
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (5, 'Multi-Answer');
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (6, 'Muliple_Choice_With_Multiple_Answers');
+INSERT INTO `QUESTION_TYPES`(`ID`, `NAME`) VALUES (7, 'Matching');
