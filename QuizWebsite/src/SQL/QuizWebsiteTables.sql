2bc5d55ac692b09c4ea9d1942e012879a13a460e
Sandro Tsiskadze <stsis15@freeuni.edu.ge>
2017-06-27 13:59:27 +0400

    ganaxlebuli baza

--- QuizWebsite/src/SQL/QuizWebsiteTables.sql
+++ QuizWebsite/src/SQL/QuizWebsiteTables.sql
@@ -4,76 +4,6 @@ default character set utf8;
 
 
 
-/**
-	ქვიზის სახელების ცხრილი
-*/
-
-CREATE TABLE `QuizWebsiteDatabase`.`QUIZ_NAMES` (
-    `ID` INT NOT NULL AUTO_INCREMENT,
-    `NAME` VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`ID`)
-);
-
-
-
-
-/**
-	კითხვის ტიპების ცხრილი
-*/
-
-CREATE TABLE `QuizWebsiteDatabase`.`QUESTION_TYPES` (
-    `ID` INT NOT NULL,
-    `NAME` VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`ID`)
-);
-
-
-  
-/**
-	კითხვების ცხრილი
-*/
-
-CREATE TABLE `QuizWebsiteDatabase`.`QUESTIONS` (
-    `ID` INT NOT NULL AUTO_INCREMENT,
-    `QUESTION` VARCHAR(400) NOT NULL,
-    `SCORE` INT NOT NULL,
-    `CHECK_TYPE` ENUM('USER', 'COMPUTER') NOT NULL,
-    `TIME` INT NOT NULL,
-    `QUIZ_ID` INT NOT NULL,
-    `TYPE_ID` INT NOT NULL,
-    PRIMARY KEY (`ID`),
-    INDEX `fk_QUESTIONS_1_idx` (`QUIZ_ID` ASC),
-    INDEX `fk_QUESTIONS_2_idx` (`TYPE_ID` ASC),
-    CONSTRAINT `fk_QUESTIONS_TO_QUIZZES` FOREIGN KEY (`QUIZ_ID`)
-        REFERENCES `QuizWebsiteDatabase`.`QUIZ_NAMES` (`ID`)
-        ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `fk_QUESTIONS_TO_TYPES` FOREIGN KEY (`TYPE_ID`)
-        REFERENCES `QuizWebsiteDatabase`.`QUESTION_TYPES` (`ID`)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-
-
-
-/**
-	პასუხების ცხრილი
-*/
-
-CREATE TABLE `QuizWebsiteDatabase`.`ANSWERS` (
-    `ID` INT NOT NULL AUTO_INCREMENT,
-    `ANSWER` VARCHAR(200) NOT NULL,
-    `T/F` ENUM('TRUE', 'FALSE') NOT NULL,
-    `ORDER` ENUM('TRUE', 'FALSE') NOT NULL,
-    `QUESTION_ID` INT NOT NULL,
-    PRIMARY KEY (`ID`),
-    INDEX `fk_ANSWERS_TO_QUESTIONS_idx` (`QUESTION_ID` ASC),
-    CONSTRAINT `fk_ANSWERS_TO_QUESTIONS` FOREIGN KEY (`QUESTION_ID`)
-        REFERENCES `QuizWebsiteDatabase`.`QUESTIONS` (`ID`)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-
-
 
 /**
 	მომხმარებლების ტიპების ცხრილი
@@ -81,7 +11,7 @@ CREATE TABLE `QuizWebsiteDatabase`.`ANSWERS` (
 
 CREATE TABLE `QuizWebsiteDatabase`.`USER_TYPES` (
     `ID` INT NOT NULL,
-    `TYPE_NAME` VARCHAR(45) NOT NULL,
+    `NAME` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`ID`)
 );
 
@@ -110,7 +40,6 @@ CREATE TABLE `QuizWebsiteDatabase`.`USERS` (
 
 
 
-
 /**
 	მეგობრების ცხრილი
 */
@@ -134,20 +63,23 @@ CREATE TABLE `QuizWebsiteDatabase`.`FRIENDS` (
 
 
 /**
-	მომხმარებლის მიერ შექმნილი ქვიზების ცხრილი
+	ქვიზის სახელების ცხრილი
 */
 
-CREATE TABLE `QuizWebsiteDatabase`.`CREATED_QUIZES` (
-	`ID` INT NOT NULL AUTO_INCREMENT,
-	`USER_ID` INT NOT NULL,
-    `QUIZ_ID` INT NOT NULL,
+CREATE TABLE `QuizWebsiteDatabase`.`QUIZZES` (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `USER_ID` INT NOT NULL,
+    `NAME` VARCHAR(45) NOT NULL,
+    `DESCRIPTION` VARCHAR(100),
+    `RANDOM` ENUM('TRUE', 'FALSE') NOT NULL,
+    `ONE_PAGE` ENUM('TRUE', 'FALSE') NOT NULL,
+    `PRACTICE_MODE` ENUM('TRUE', 'FALSE') NOT NULL,
+    `IMMEDIATE_GRADE` ENUM('TRUE', 'FALSE') NOT NULL,
+    `FREQUENCY` INT NOT NULL,
     PRIMARY KEY (`ID`),
-     INDEX `fk_CREATED_QUIZES_1_idx` (`USER_ID` ASC),
-     INDEX `fk_CREATED_QUIZES_2_idx` (`QUIZ_ID` ASC),
-    CONSTRAINT `fk_CREATED_QUIZES_TO_USERS` FOREIGN KEY (`USER_ID`)
-        REFERENCES `QuizWebsiteDatabase`.`USERS` (`ID`),
-    CONSTRAINT `fk_CREATED_QUIZES_TO_QUIZES` FOREIGN KEY (`QUIZ_ID`)
-        REFERENCES `QuizWebsiteDatabase`.`QUIZ_NAMES` (`ID`)
+    INDEX `fk_QUIZZES_1_idx` (`USER_ID` ASC),
+    CONSTRAINT `fk_QUIZZES_TO_USERS` FOREIGN KEY (`USER_ID`)
+		REFERENCES `QuizWebsiteDatabase`.`USERS` (`ID`)
         ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
@@ -170,7 +102,65 @@ CREATE TABLE `QuizWebsiteDatabase`.`COMPLETED_QUIZES` (
     CONSTRAINT `fk_COMPLETED_QUIZES_TO_USERS` FOREIGN KEY (`USER_ID`)
         REFERENCES `QuizWebsiteDatabase`.`USERS` (`ID`),
     CONSTRAINT `fk_COMPLETED_QUIZES_TO_QUIZES` FOREIGN KEY (`QUIZ_ID`)
-        REFERENCES `QuizWebsiteDatabase`.`QUIZ_NAMES` (`ID`)
+        REFERENCES `QuizWebsiteDatabase`.`QUIZZES` (`ID`)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
+
+
+/**
+	კითხვის ტიპების ცხრილი
+*/
+
+CREATE TABLE `QuizWebsiteDatabase`.`QUESTION_TYPES` (
+    `ID` INT NOT NULL,
+    `NAME` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`ID`)
+);
+
+
+
+
+/**
+	კითხვების ცხრილი
+*/
+
+CREATE TABLE `QuizWebsiteDatabase`.`QUESTIONS` (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `QUESTION` VARCHAR(400) NOT NULL,
+    `SCORE` INT NOT NULL,
+    `CHECK_TYPE` ENUM('USER', 'COMPUTER') NOT NULL,
+    `TIME` INT NOT NULL,
+    `QUIZ_ID` INT NOT NULL,
+    `TYPE_ID` INT NOT NULL,
+    PRIMARY KEY (`ID`),
+    INDEX `fk_QUESTIONS_1_idx` (`QUIZ_ID` ASC),
+    INDEX `fk_QUESTIONS_2_idx` (`TYPE_ID` ASC),
+    CONSTRAINT `fk_QUESTIONS_TO_QUIZZES` FOREIGN KEY (`QUIZ_ID`)
+        REFERENCES `QuizWebsiteDatabase`.`QUIZZES` (`ID`)
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_QUESTIONS_TO_TYPES` FOREIGN KEY (`TYPE_ID`)
+        REFERENCES `QuizWebsiteDatabase`.`QUESTION_TYPES` (`ID`)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
+
+/**
+	პასუხების ცხრილი
+*/
+
+CREATE TABLE `QuizWebsiteDatabase`.`ANSWERS` (
+    `ID` INT NOT NULL AUTO_INCREMENT,
+    `ANSWER` VARCHAR(200) NOT NULL,
+    `TF` ENUM('TRUE', 'FALSE') NOT NULL,
+    `ORDER` ENUM('TRUE', 'FALSE') NOT NULL,
+    `QUESTION_ID` INT NOT NULL,
+    PRIMARY KEY (`ID`),
+    INDEX `fk_ANSWERS_TO_QUESTIONS_idx` (`QUESTION_ID` ASC),
+    CONSTRAINT `fk_ANSWERS_TO_QUESTIONS` FOREIGN KEY (`QUESTION_ID`)
+        REFERENCES `QuizWebsiteDatabase`.`QUESTIONS` (`ID`)
         ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
@@ -183,10 +173,9 @@ CREATE TABLE `QuizWebsiteDatabase`.`COMPLETED_QUIZES` (
 
 CREATE TABLE `QuizWebsiteDatabase`.`ACHIEVEMENT_TYPES` (
     `ID` INT NOT NULL,
-    `TYPE_NAME` VARCHAR(45) NOT NULL,
+    `NAME` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`ID`)
 );
-
 
 
 
