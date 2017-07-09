6efd58312617cc8639f87289644b6976c129d480
levangasviani <lgasv15@freeuni.edu.ge>
2017-07-09 21:35:00 +0400

    დავამატე ქვიზის დაწყების სერვლეტი

---
+++ QuizWebsite/src/Quiz/StartQuizServlet.java
@@ -0,0 +1,51 @@
+package Quiz;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import WebSite.WebSiteInfo;
+
+/**
+ * Servlet implementation class StartQuizServlet
+ */
+@WebServlet("/StartQuizServlet")
+public class StartQuizServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StartQuizServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String quizName = request.getParameter("quizName");
+		QuizManager qm = (QuizManager) this.getServletContext().getAttribute(WebSiteInfo.QUIZ_MANAGER_ATTR);
+		Quiz quiz = qm.getQuiz(quizName);
+		request.setAttribute("quizName", quiz.getName());
+		request.setAttribute("description", quiz.getDescription());
+		RequestDispatcher rd=request.getRequestDispatcher("StartQuiz.jsp");
+		rd.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
