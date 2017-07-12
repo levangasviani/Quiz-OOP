fc63d0b34213a1086430987477a3014c6097b7d6
levangasviani <lgasv15@freeuni.edu.ge>
2017-07-12 17:00:58 +0400

    ჩავამატე კონსტანტა რომლის მნიშვნელობის მიხედვითაც დაგენერირდება homepage-ზე შესაბამისი განყოფილებები

--- QuizWebsite/src/Database/DBInfo.java
+++ QuizWebsite/src/Database/DBInfo.java
@@ -8,7 +8,7 @@ package Database;
 public class DBInfo {
 
 	public static final String MYSQL_USERNAME = "root";
-	public static final String MYSQL_PASSWORD = "";
+	public static final String MYSQL_PASSWORD = "sius25";
 	public static final String MYSQL_DATABASE_NAME = "QuizWebsiteDatabase";
 	public static final String MYSQL_DATABASE_SERVER = "localhost:3306/" + MYSQL_DATABASE_NAME
 			+ "?autoReconnect=true&useSSL=false";
@@ -149,4 +149,7 @@ public class DBInfo {
 
 	// MAX ID Value
 	public static final int MAX_ID_VALUE = 2147483647;
+	
+	// Default quantity for home page information
+	public static final int DEFAULT_NUMBER_OF_STATS = 5;
 }
