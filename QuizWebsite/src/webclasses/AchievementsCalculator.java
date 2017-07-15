e028c2b4f091151c5fcaa687be7d4bfbcda3b958
levanAmateur <lkara15@freeuni.edu.ge>
2017-07-15 15:56:06 +0400

    achievementsmanager davumate seter metodi, davwere achievementscalculator, tumca ara bolomde radgan velodebi statistikis damtvlel class

---
+++ QuizWebsite/src/webclasses/AchievementsCalculator.java
@@ -0,0 +1,48 @@
+package webclasses;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author levanAmateur (lkara15)
+ *
+ */
+public class AchievementsCalculator {
+	
+	/**
+	 * Constructor
+	 */
+	public AchievementsCalculator() {
+		
+	}
+	
+	/**
+	 * Calculates achievements of user according
+	 * to given paramaters
+	 * 
+	 * @param numOfCreatedQuizzes
+	 * @param numOfCompletedQuizzes
+	 * @return
+	 */
+	public ArrayList<String> getAchievements(int numOfCreatedQuizzes, int numOfCompletedQuizzes) {
+		ArrayList<String> result = new ArrayList<String>();
+		if(numOfCompletedQuizzes >= 1) {
+			result.add("Amateur Author");
+		}
+		if(numOfCompletedQuizzes >= 5) {
+			result.add("Prolific Author");
+		}
+		if(numOfCompletedQuizzes >= 10) {
+			result.add("Prodigious Author");
+		}
+		
+		if(numOfCompletedQuizzes >= 10) {
+			result.add("Quiz Machine");
+		}
+		return result;
+	}
+	
+}
+
+
+
