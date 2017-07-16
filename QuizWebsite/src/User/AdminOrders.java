5c64ea5f977b4803509ac8d8c8548f63d05ad3da
shotakobaxidze <skoba15@freeuni.edu.ge>
2017-07-16 12:33:16 +0300

    statikuri cvladebi adminis operaciebistvis

---
+++ QuizWebsite/src/User/AdminOrders.java
@@ -0,0 +1,9 @@
+package User;
+
+public class AdminOrders {
+
+	public static final int ADD_ANNOUNCEMENT = 1;
+	public static final int REMOVE_ACCOUNT = 2;
+	public static final int REMOVE_QUIZ = 3;
+	public static final int PROMOTE = 4;
+}
